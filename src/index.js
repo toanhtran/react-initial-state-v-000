0de37546b8f25a00238c4e1125ada0a06e55fdbb
ToAnh Tran <toanh.t.tran@gmail.com>
2019-03-08 22:13:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
 
 ReactDOM.render(
   <ToggleButton />, document.getElementById('root')
