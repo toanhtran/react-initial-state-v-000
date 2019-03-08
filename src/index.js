e6695e1adf66b7c2ce5f3a22ec1f7654be728623
ToAnh Tran <toanh.t.tran@gmail.com>
2019-03-08 22:20:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
+
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
-  document.getElementById('root')
-)
+  <Address
+  street ="Santa Monica Blvd."
+  city="Santa Monica" />, document.getElementById('root');
+  <ToggleButton />, document.getElementById('root')
+);
